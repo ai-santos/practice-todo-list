c5fbed3d1bf1b2f24b4ce3bea2de6b810d9455b7
Aileen <asantos3026@gmail.com>
2017-05-09 22:25:06 -0700

    Setup complete

---
+++ src/server.js
@@ -0,0 +1,21 @@
+import express from 'express'
+import index from './routes/index'
+import path from 'path'
+import logger from 'morgan'
+import bodyParser from 'body-parser'
+
+const server = express()
+
+//middleware
+server.set('views', path.join(__dirname, 'views'))
+server.set('view engine', 'ejs')
+
+server.use(logger('dev'))
+server.use(express.static(path.join(__dirname + '/public')))
+server.use(bodyParser.urlencoded({extended: true}))
+
+server.use('/', index)
+
+server.listen(process.env.PORT || 8080)
+
+module.exports = server
